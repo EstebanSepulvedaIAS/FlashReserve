885f4c88a3d89faa3b5c1e3ea695becf23177b1c
Esteban Sepulveda Fierro <esteban.sepulveda@ias.com.co>
2026-09-16 18:18:56 -0500

    Feature(DB) add mongodb context and docker container

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FlashReserve
 Application for Company Flash Reserve
+
+# Base de datos
+El proyecto utiliza MongoDB como base de datos. La configuración necesaria para ejeuctarse mediante Docker se encuentra en la carpeta db/
+
+## Comandos para ejecutarse
+
+1. Deberá ejecutar si así lo desea desde la raíz del proyecto: docker compose -f db/docker-compose.yml up -d para iniciar el contenedor 
+2. Ejecutar docker compose logs -f mongo-init para validar que haya subido correctamente la base de datos y que al final muestre un mensaje que diga "Base de datos de FlashReserve lista para usar"
+3. Ejecutar docker exec -it flashreserve-mongo mongosh flashreserve_db para ingresar a la base de datos
+4. Ejecutar db.skus.find(); para validar que haya guardado correctamente la información de prueba
+5. Ejecutar db.reservations.getIndexes(); para validar que se hayan creado los índices
---
+++ db/init-scripts/01-init-data.js
@@ -0,0 +1,54 @@
+print("Inicio de configuración de Mongo");
+
+try{
+    rs.initiate({
+        _id: "rs0",
+        members: [{ _id: 0, host: "mongodb:27017"}]
+    });
+    print("Replica set configurado");
+}catch (e){
+    print("Replica set ya estaba iniciado o hubo un error: " + e);
+}
+print("Esperando elección de nodo PRIMARY");
+while(!db.hello().isWritablePrimary){
+    sleep(1000);
+    print("Aún no es primary, esperando 1 segundo");
+}
+print("El nodo ahora es primary");
+
+db = db.getSiblingDB('flashreserve_db');
+
+print("Creando colecciones e indices");
+
+db.createCollection('skus');
+db.createCollection('reservations');
+db.createCollection('processed_events');
+
+db.reservations.createIndex({ "idempotencyKey": 1 }, { unique: true} );
+db.reservations.createIndex({ "customerId": 1 });
+db.reservations.createIndex({ "status": 1 });
+db.reservations.createIndex({ "sku": 1 });
+
+db.processed_events.createIndex({ "reservationId": 1 });
+
+print("Insertando inventario incial en skus");
+
+try{
+    db.skus.insertMany([
+        {
+            _id: "IPHONE-15",
+            available: 100,
+            reserved: 0
+        },
+        {
+            _id: "MACKBOOK-AIR",
+            available: 5,
+            reserved: 0
+        }
+    ]);
+    print("SKUs de prueba insertados");
+}catch(e){
+    print("Los datos de prueba ya existian o hubo error: " + e);
+}
+
+print("Base de datos de FlashReserve lista para usar");
